5dc0a563df80bc3a329aa266ba4cc318dfc28414
pocy <1254778746@qq.com>
2020-11-22 16:53:47 +0800

    α迭代测试版

---
+++ components/star/star.js
@@ -0,0 +1,43 @@
+// component/rating/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+ 
+  },
+ 
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgs: [{
+      id: 1
+    }, {
+      id: 2
+    }, {
+      id: 3
+    }, {
+      id: 4
+    }, {
+      id: 5
+    }],
+    starId: 5,
+    src1: 'https://img-blog.csdnimg.cn/20200708144629175.png',
+    src2: 'https://img-blog.csdnimg.cn/20200708144629178.png',
+  },
+ 
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    select(e) {
+      console.log(e)
+      this.data.starId = e.currentTarget.dataset.index;
+      this.setData({
+        starId: this.data.starId
+      })
+    }
+  }
+})
+
